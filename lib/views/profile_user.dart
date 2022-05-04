b59bf7058ada47984f5372e53a62ae745b0f2cea
Jvilchezar10 <86031563+Jvilchezar10@users.noreply.github.com>
2022-05-03 23:02:55 -0500

    .:::

--- lib/views/profile_user.dart
+++ lib/views/profile_user.dart
@@ -10,7 +10,7 @@ class UserProfileView extends StatefulWidget {
 }
 
 class _UserProfilePageState extends State<UserProfileView> {
-  final User usuario = User.users.first;
+  final User usuario = User.users.last;
 
   @override
   Widget build(BuildContext context) {
@@ -20,47 +20,125 @@ class _UserProfilePageState extends State<UserProfileView> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
+            Header(
+              usuario: usuario,
               height: 190,
-              padding: const EdgeInsets.only(bottom: 10),
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("graphics/images/fondo_user.jpg"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
+            ),
+            Container(
+              color: Colors.pink,
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Container(
-                    width: 120,
-                    height: 120,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(usuario.imageUrls[0]),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'Edad',
+                        style: themedark()
+                            .textTheme
+                            .headline3
+                            ?.copyWith(color: Colors.black),
                       ),
-                      //image: const DecorationImage(
-                      //image: AssetImage("graphics/images/user.jpg"),
-                      //fit: BoxFit.cover,
-                      // ),
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 4,
+                      Text(
+                        usuario.age.toString(),
+                        style: themedark()
+                            .textTheme
+                            .headline3
+                            ?.copyWith(color: Colors.black),
                       ),
-                    ),
+                    ],
                   ),
-                  Text(
-                    usuario.name, //nombre de usuario
-                    style: themedark()
-                        .textTheme
-                        .headline3
-                        ?.copyWith(color: Colors.white),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        "Sexo",
+                        style: themedark()
+                            .textTheme
+                            .headline3
+                            ?.copyWith(color: Colors.black),
+                      ),
+                      Text(
+                        usuario.gender,
+                        style: themedark()
+                            .textTheme
+                            .headline3
+                            ?.copyWith(color: Colors.black),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  final User usuario;
+  final double height;
+
+  const Header({
+    Key? key,
+    required this.usuario,
+    this.height = 190,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      padding: const EdgeInsets.only(bottom: 10),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("graphics/images/fondo_user.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          UserPhoto(usuario: usuario, size: 120),
+          Text(
+            "@" + (usuario.name), //nombre de usuario
+            style:
+                themedark().textTheme.headline3?.copyWith(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class UserPhoto extends StatelessWidget {
+  final double size;
+  final User usuario;
+
+  const UserPhoto({
+    Key? key,
+    required this.usuario,
+    this.size = 120,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(usuario.imageUrls[0]),
+        ),
+        //image: const DecorationImage(
+        //image: AssetImage("graphics/images/user.jpg"),
+        //fit: BoxFit.cover,
+        // ),
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.white,
+          width: 4,
         ),
       ),
     );
