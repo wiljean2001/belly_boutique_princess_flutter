0b587f88b56bb1d8e2488681f027064c2b26eb14
Jvilchezar10 <86031563+Jvilchezar10@users.noreply.github.com>
2022-05-03 20:11:49 -0500

    .:.:

--- lib/views/profile_user.dart
+++ lib/views/profile_user.dart
@@ -1,4 +1,5 @@
 import 'package:belly_boutique_princess/config/theme_dark.dart';
+import 'package:belly_boutique_princess/models/user_model.dart';
 import 'package:flutter/material.dart';
 
 class UserProfileView extends StatefulWidget {
@@ -9,19 +10,55 @@ class UserProfileView extends StatefulWidget {
 }
 
 class _UserProfilePageState extends State<UserProfileView> {
+  final User usuario = User.users.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              "Hola",
-              style: themedark()
-                  .textTheme
-                  .headline3
-                  ?.copyWith(color: Colors.black),
+            Container(
+              height: 190,
+              padding: const EdgeInsets.only(bottom: 10),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("graphics/images/fondo_user.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(usuario.imageUrls[0]),
+                      ),
+                      //image: const DecorationImage(
+                      //image: AssetImage("graphics/images/user.jpg"),
+                      //fit: BoxFit.cover,
+                      // ),
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 4,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    usuario.name, //nombre de usuario
+                    style: themedark()
+                        .textTheme
+                        .headline3
+                        ?.copyWith(color: Colors.white),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
