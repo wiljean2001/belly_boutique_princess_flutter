a5c37bcb3749e8c3eeb85aba4c87080d788596e3
wiljean2001 <86029273+wiljean2001@users.noreply.github.com>
2022-05-08 18:13:19 -0500

    intl_es.arb

---
+++ lib/generated/intl/messages_es.dart
@@ -0,0 +1,72 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a es locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'es';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "AppTitle":
+            MessageLookupByLibrary.simpleMessage("Bely Boutique Princess"),
+        "COMMENT1": MessageLookupByLibrary.simpleMessage(" // HOME SCREENS "),
+        "COMMENT2":
+            MessageLookupByLibrary.simpleMessage(" // ONBOARDING SCREENS "),
+        "Error_displaying_interaces": MessageLookupByLibrary.simpleMessage(
+            "Error al cargar las interfaces"),
+        "bttn_date_birth": MessageLookupByLibrary.simpleMessage(
+            "Seleccionar fecha de nacimiento"),
+        "bttn_go_back": MessageLookupByLibrary.simpleMessage("Regresar"),
+        "bttn_login": MessageLookupByLibrary.simpleMessage("Ingresar"),
+        "bttn_register": MessageLookupByLibrary.simpleMessage("Registrarse"),
+        "bttn_start": MessageLookupByLibrary.simpleMessage("Empezar"),
+        "description_app": MessageLookupByLibrary.simpleMessage(
+            "Esta es una tienda que se dedica a vender vestidos para las princesas de casa."),
+        "email": MessageLookupByLibrary.simpleMessage("Correo electrónico"),
+        "error_desc": MessageLookupByLibrary.simpleMessage("Algo salió mal"),
+        "error_title": MessageLookupByLibrary.simpleMessage("Error"),
+        "gender": MessageLookupByLibrary.simpleMessage("Sexo"),
+        "gender_female": MessageLookupByLibrary.simpleMessage("Femenino"),
+        "gender_male": MessageLookupByLibrary.simpleMessage("Masculino"),
+        "logo": MessageLookupByLibrary.simpleMessage(
+            "graphics/images/LOGO4-small.png"),
+        "logo_home": MessageLookupByLibrary.simpleMessage(
+            "graphics/images/bely_logo-edit.png"),
+        "menu_appbar_item1": MessageLookupByLibrary.simpleMessage("Visitanos"),
+        "menu_appbar_item2":
+            MessageLookupByLibrary.simpleMessage("Configuracion"),
+        "menu_appbar_item3": MessageLookupByLibrary.simpleMessage("Ayuda"),
+        "menu_appbar_item4":
+            MessageLookupByLibrary.simpleMessage("Cerrar sesión"),
+        "password": MessageLookupByLibrary.simpleMessage("Contraseña"),
+        "register_error_desc":
+            MessageLookupByLibrary.simpleMessage("Usuario existente"),
+        "register_error_title":
+            MessageLookupByLibrary.simpleMessage("Error de registro"),
+        "title_app": MessageLookupByLibrary.simpleMessage("Bienvenido"),
+        "title_user_screen": MessageLookupByLibrary.simpleMessage("Usuario"),
+        "tooltip_bttn_options":
+            MessageLookupByLibrary.simpleMessage("Opciones"),
+        "tooltip_bttn_shopping_card":
+            MessageLookupByLibrary.simpleMessage("Carrito"),
+        "validator_email_error":
+            MessageLookupByLibrary.simpleMessage("Correo no válido"),
+        "validator_password_error":
+            MessageLookupByLibrary.simpleMessage("Contraseña no válida")
+      };
+}
